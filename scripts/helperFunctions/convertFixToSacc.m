3f7fd3ad6ba9e4a833aa30d4943f90ec36ef26c5
Alasdair Clarke <a.clarke@abdn.ac.uk>
2015-06-16 09:18:32 +0100

    A load of small fixes

--- scripts/helperFunctions/convertFixToSacc.m
+++ scripts/helperFunctions/convertFixToSacc.m
@@ -7,7 +7,7 @@ T = unique(fix(:,2));
 for p = 1:length(P)
     for t = 1:length(T)
         tfix = fix(find((fix(:,1)==P(p)).*(fix(:,2)==T(t))),:);
-        for f = 2:length(tfix)
+        for f = 3:length(tfix)
            saccs = [saccs;  tfix(f-1,4:5), tfix(f,4:5)];
         end
         clear tfix
