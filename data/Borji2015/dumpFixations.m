b51c6b7b9368d3a29977220eee2f0d2e5e724e95
Alasdair Clarke <a.clarke@abdn.ac.uk>
2015-10-04 10:13:58 +0100

    Split Borji2015 by class

--- data/Borji2015/dumpFixations.m
+++ data/Borji2015/dumpFixations.m
@@ -2,17 +2,43 @@ clear all
 close all
 
 load allFixData
-allDat = [];
+allFixDat = containers.Map('KeyType','char','ValueType','any');
+
 K = keys(allData);
+class = cell(1,2000);
+ii = 0;
+for scn=1:length(K);
+    ii = ii + 1;
+    k = K{scn};
+    tmp = regexp(k, '[\w]*(?=/)', 'match');
+    class{ii} = tmp{1};
+end
+clear k ii tmp
+classes = unique(class);
+clear class
+
+for cls = 1:length(classes)
+    class = classes{cls};
+    allFixDat(class) = [];
+end
+
 
 for scn=1:length(K);
     k = K{scn};
     dat = allData(k);
+    tmp = regexp(k, '[\w]*(?=/)', 'match');
+    class = tmp{1};
+    clear tmp
     for ii = 1:length(dat)
         if size(dat{ii}.data,1) > 1
-            allDat = [allDat; [repmat(scn, [length(dat{ii}.data), 1]), repmat(ii, [length(dat{ii}.data), 1]), dat{ii}.data]];
+            
+            allFixDat(class) = [allFixDat(class); [repmat(scn, [length(dat{ii}.data), 1]), repmat(ii, [length(dat{ii}.data), 1]), dat{ii}.data]];
         end
     end
 end
 
-dlmwrite('allFixations.txt', allDat);
+for cls = 1:length(classes)
+    class = classes{cls};
+    classFixDat = allFixDat(class);
+    dlmwrite([class 'Fixations.txt'], classFixDat);
+end
