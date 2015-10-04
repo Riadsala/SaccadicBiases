549e93eb75c50d1701020ad724e5915529dc7780
Alasdair Clarke <a.clarke@abdn.ac.uk>
2015-10-04 10:32:52 +0100

    Borji saccades

--- scripts/helperFunctions/getSaccades.m
+++ scripts/helperFunctions/getSaccades.m
@@ -3,7 +3,7 @@ close all
 
 addpath('../helperfunctions');
 
-% 
+%
 % datasets = {
 %     'Asher2013',...
 %     'Clarke2013', ...
@@ -29,26 +29,40 @@ for (d = datasets)
     else
         aspectRatio=0.75;
     end
-        
+    
+    % import fixations
     fix = grabFixations(d{1});
-    saccs1 = convertFixToSacc(fix, aspectRatio);
     
-    
-    dlmwrite(['../../data/saccs/' d{1} 'saccs.txt'], saccs1);
-    
-    fixF = fix;
-    fixF.x = -fixF.x;
-    saccs2 = convertFixToSacc(fixF, aspectRatio);
-    fixF = fix;
-    fixF.y = -fixF.y;
-    saccs3 = convertFixToSacc(fixF, aspectRatio);
-    fixF = fix;
-    fixF.x = -fixF.x;
-    fixF.y = -fixF.y;
-    saccs4 = convertFixToSacc(fixF, aspectRatio);
-    saccs = [saccs1; saccs2;saccs3;saccs4];
-    dlmwrite(['../../data/saccs/' d{1} 'saccsMirrored.txt'], saccs);
-    
+    if strcmp(d, 'Borji2015')
+        % This is a multi class dataset
+        % get class names
+         sets = dir('../../data/Borji2015/*.txt');
+        for cls = 1:length(fix)
+            classname = regexp(sets(cls).name, '[\w]*(?=Fix)', 'match')
+            saccs1 = convertFixToSacc(fix(cls), aspectRatio);
+            
+            dlmwrite(['../../data/saccs/' d{1} '_' classname{1} 'saccs.txt'], saccs1);
+            clear saccs1
+        end
+        
+    else
+        
+        
+        saccs1 = convertFixToSacc(fix, aspectRatio);        
+        dlmwrite(['../../data/saccs/' d{1} 'saccs.txt'], saccs1);     
+        fixF = fix;
+        fixF.x = -fixF.x;
+        saccs2 = convertFixToSacc(fixF, aspectRatio);
+        fixF = fix;
+        fixF.y = -fixF.y;
+        saccs3 = convertFixToSacc(fixF, aspectRatio);
+        fixF = fix;
+        fixF.x = -fixF.x;
+        fixF.y = -fixF.y;
+        saccs4 = convertFixToSacc(fixF, aspectRatio);
+        saccs = [saccs1; saccs2;saccs3;saccs4];
+        dlmwrite(['../../data/saccs/' d{1} 'saccsMirrored.txt'], saccs);
+    end
     
     
 end
